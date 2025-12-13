16695f05c1e6ad0a9f41852301b923163b124d44
BogdanKCB <kapachenya.bogdan@gmail.com>
2025-12-13 15:09:19 +0300

    Добавьте файлы проекта.

---
+++ Lab3_Variant1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab3_Variant1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
